d062ef8dd9b019295bd5e240367913a5c49660b7
AloeFlawinGulenywa <aloeflawingulenywa98@gmail.com>
2023-09-27 06:13:01 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+
+// function isOdd(element, index, array) {
+//     return (element % 2 === 1);
+//   }
+
+function superbowlWIn(record) {
+
+    // let result = record.find( record => record.result === "W")
+    // return !!result ? result.year : undefined
+    const result = record.find( record => record.result === "W");
+    return !!result ? result.year : undefined;
+}
+const result = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  //...
+]
+console.log(result);
